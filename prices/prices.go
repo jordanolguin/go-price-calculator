3dafa32bb4445c1006c4ba9e40d9f9be1d74962e
Jordan Olguin <jdolguin62@gmail.com>
2025-11-04 08:02:45 -0700

    outsourcing file acces into a package

--- prices/prices.go
+++ prices/prices.go
@@ -1,11 +1,10 @@
 package prices
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 
 	"example.com/price-calculator/conversion"
+	"example.com/price-calculator/filemanager"
 )
 
 type TaxIncludedPriceJob struct {
@@ -15,41 +14,23 @@ type TaxIncludedPriceJob struct {
 }
 
 func (job *TaxIncludedPriceJob) LoadData() {
-	file, err := os.Open("prices.txt")
-	if err != nil {
-		fmt.Println("Could not open file!")
-		fmt.Println(err)
-		return
-	}
-
-	scanner := bufio.NewScanner(file)
-
-	var lines []string
-
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-
-	err = scanner.Err()
+	
+	lines, err := filemanager.ReadLines("prices.txt")
 
 	if err != nil {
-		fmt.Println("Reading the file content failed.")
 		fmt.Println(err)
-		file.Close()
 		return
 	}
 
 	prices, err := conversion.StringsToFloat(lines)
 
-		if err != nil {
-			fmt.Println(err)
-			file.Close()
-			return
-		}
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
  
 		job.InputPrices = prices
-		file.Close()
-	}
+}
 
 
 func (job *TaxIncludedPriceJob) Process() {
